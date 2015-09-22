422f558afd1ef10cc33e244d106e93785d8bf538
Hage Yaapa <captain@hacksparrow.com>
2015-09-22 14:29:38 +0530

    review by bajtos

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ app.start = function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-explorer')) {
-      var explorerPath = app.get('loopback-explorer').mountPath;
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
